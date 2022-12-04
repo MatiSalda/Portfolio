2670bb57272da90c8cbc6e4b99f13ebb83637f20
Matias Saldaño <matiasnsalda@gmail.com>
2022-12-03 23:22:10 -0300

    actualizacion

--- script.js
+++ script.js
@@ -7,5 +7,21 @@ window.addEventListener('scroll', () => {
 
 addEventListener('click', () =>{
     const botonDark = document.getElementById('botonDark')
-    
 })
+
+// header
+const botonMenu = document.getElementById('menuHamburguesa')
+const lineaMenu1 = document.getElementById('linea1')
+const lineaMenu2 = document.getElementById('linea2')
+const lineaMenu3 = document.getElementById('linea3')
+const navbar = document.getElementById('navbar')
+
+botonMenu.addEventListener('click', ()=>{
+lineaMenu1.classList.toggle('activeLineaMenu1')
+lineaMenu2.classList.toggle('activeLineaMenu2')
+lineaMenu3.classList.toggle('activeLineaMenu3')
+navbar.classList.toggle('activeNavbar')
+
+})
+
+// header
