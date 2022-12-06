2c9b16de1aff44419002502bd30e84520c8f2318
Matias Saldaño <matiasnsalda@gmail.com>
2022-12-06 09:42:41 -0300

    update header mobile

--- script.js
+++ script.js
@@ -5,15 +5,14 @@ window.addEventListener('scroll', () => {
     header.classList.toggle('abajo', window.scrollY > 0);
 });
 
-addEventListener('click', () =>{
-    const botonDark = document.getElementById('botonDark')
-})
 
 // header
+
 const botonMenu = document.getElementById('menuHamburguesa')
 const lineaMenu1 = document.getElementById('linea1')
 const lineaMenu2 = document.getElementById('linea2')
 const lineaMenu3 = document.getElementById('linea3')
+const anchor = document.getElementById('anchor')
 const navbar = document.getElementById('navbar')
 
 botonMenu.addEventListener('click', ()=>{
@@ -21,7 +20,15 @@ lineaMenu1.classList.toggle('activeLineaMenu1')
 lineaMenu2.classList.toggle('activeLineaMenu2')
 lineaMenu3.classList.toggle('activeLineaMenu3')
 navbar.classList.toggle('activeNavbar')
+// anchor.classList.toggle('activeNavbarAnimation')
 
 })
-
 // header
+
+const botonDark = document.getElementById('botonDark')
+const body = document.getElementById('main')
+botonDark.addEventListener('click', () =>{
+    body.classList.toggle('darkMode')
+    botonDark.classList.toggle('btnLightMode')
+    
+})   
